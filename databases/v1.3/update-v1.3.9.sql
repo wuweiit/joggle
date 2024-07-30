338d275ea62d6b015921a6df19bab501b379b319
marker <admin@wuweibi.com>
2024-07-30 22:38:47 +0800

    [1.3.9] 用户新增套餐标识，支持套餐搜索

--- databases/v1.3/update-v1.3.9.sql
+++ databases/v1.3/update-v1.3.9.sql
@@ -9,3 +9,7 @@ update client_version set type = 'CLIENT'  where title like 'JoggleClient%';
 
 ALTER TABLE `t_server_tunnel`
     ADD COLUMN `version` varchar(20) NULL COMMENT '通道版本' AFTER `server_addr`;
+
+
+ALTER TABLE `user_package`
+    ADD INDEX `idx_packageId`(`resource_package_id`);
